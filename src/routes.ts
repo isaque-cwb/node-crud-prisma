4501222a19b6b0f64f9fc7629fbde82b23f732ec
Isaque Lourenço <isaque.lourenco2@gmail.com>
2022-02-27 13:17:50 -0300

    Implementado inserção de categorias

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+import { CreateCategoryController } from './controllers/createCategoryController'
 import { CreateProductController } from './controllers/createProductController'
 
 
@@ -8,7 +9,9 @@ export const router = Router()
 
 
 const createProduct = new CreateProductController()
+const createCategory = new CreateCategoryController()
 
 router.post('/product', createProduct.handle)
+router.post('/category', createCategory.handle)
 
 
